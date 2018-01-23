dc72271866e41be5483e53b1af9505ad23d41746
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2018-01-23 10:17:53 +0500

    pres. components

---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-detail',
+  template: `
+    <div>
+      Passenger Detail
+    </div>
+  `
+})
+export class PassengerDetailComponent {
+  constructor() {
+  }
+}
