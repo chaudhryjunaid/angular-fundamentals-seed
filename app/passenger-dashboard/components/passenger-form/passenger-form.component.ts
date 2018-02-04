d138c8e60a693233a4468a9fa9314869f7e2e5bd
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2018-02-04 11:39:53 +0500

    radio buttons

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -16,12 +16,44 @@ import { Passenger } from "../../models/passenger.interface";
             [ngModel]="detail?.fullname"
           >
         </div>
+        
         <div>
           Passenger ID:
           <input
             type="number"
             name="id"
             [ngModel]="detail?.id"
+          >
+        </div>
+        
+        <div>
+          <label>
+            <input
+              type="radio"
+              [value]="true"
+              name="checkedIn"
+              [ngModel]="detail?.checkedIn"
+              (ngModelChange)="toggleCheckedIn($event)"
+              >
+            Yes
+          </label>
+          <label>
+            <input
+              type="radio"
+              [value]="false"
+              name="checkedIn"
+              [ngModel]="detail?.checkedIn"
+              (ngModelChange)="toggleCheckedIn($event)"
+            >
+            No
+          </label>
+        </div>
+        
+        <div *ngIf="form.value.checkedIn">
+          <input
+            type="number"
+            name="checkInDate"
+            [ngModel]="detail?.checkInDate"
           >
         </div>
       </form>
@@ -33,4 +65,9 @@ export class PassengerFormComponent {
   @Input()
   detail: Passenger;
   constructor() {}
+  toggleCheckedIn(checkedIn: boolean) {
+    if(checkedIn) {
+      this.detail.checkInDate = Date.now();
+    }
+  }
 }
